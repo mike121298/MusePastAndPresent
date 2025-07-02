5e3c236d4ed9611ee461f2fa34646952937486a2
mike121298 <mikemike.121298@gmail.com>
2025-07-02 13:35:40 +0800

    new

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-MusePastAndPresent-v0.15";
+const cacheName = "DefaultCompany-MusePastAndPresent-v0.16";
 const contentToCache = [
     "Build/MusePastAndPresent.loader.js",
     "Build/MusePastAndPresent.framework.js",
